fff6817e360219b724e1ffa42549249d8a660dde
TheRamy <122477951+TheRamy@users.noreply.github.com>
2023-02-24 09:21:44 +0100

    Signup/Login UI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from time import time
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
@@ -14,7 +16,64 @@ class LoginUI(QDialog):
         loadUi("./UI/login.ui",self)
 
         # This is example of changing screen
-        self.loginButton.clicked.connect(self.go_main_menu)
+        self.loginButton.clicked.connect(self.logIN)
+
+        self.signUpButton.clicked.connect(self.signUp)
+
+        # hide error messaages in the GUI by default
+        self.errorTextSignUp.setText("")
+        self.errorTextLogin.setText("")
+
+
+    def logIN (self):
+        if self.emailInputLogin.text() :
+            # Load the JSON file
+            with open('users.json', 'r') as file:
+                users_data = json.load(file)
+
+            # Check if the new user already exists
+
+            for user in users_data['UsersDB'].values():
+                if user['Email'] == self.emailInputLogin.text():
+                    self.go_main_menu()
+                    break
+            else:
+                self.errorTextLogin.setText("Email does not exist!")
+        else:
+            self.errorTextLogin.setText("Email can't be empty")
+
+    def signUp (self) :
+
+        name = self.nameInputSignUp.text()
+        new_user_email = self.emailInputSignUp.text()
+
+        if name and new_user_email :
+
+            # Load the JSON file
+            with open('users.json', 'r') as file:
+                users_data = json.load(file)
+
+            # Check if the new user already exists
+
+            for user in users_data['UsersDB'].values():
+                if user['Email'] == new_user_email:
+                    self.errorTextSignUp.setText("User with email address already exists.")
+
+                    break
+            else:
+                # Add the new user
+                new_user_id = str(len(users_data['UsersDB']) + 1)
+                new_user = {'Email': new_user_email, 'Name': name}  
+                users_data['UsersDB'][new_user_id] = new_user
+
+                # Save the updated JSON file
+                with open('users.json', 'w') as file:
+                    json.dump(users_data, file, indent=4)
+                
+                self.errorTextSignUp.setText("New user added, you may login!")
+        else:
+            self.errorTextSignUp.setText("Name or email is empty!")
+
 
     def go_main_menu(self):
         main_menu = MainMenuUI()
